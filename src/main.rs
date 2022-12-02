3937d0712f5bd02b2b3a48f7fb53af10a5c63b85
JaimeNA <nam.jaime@gmail.com>
2022-12-01 21:59:25 -0300

    Started with the 4004

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,23 @@ enum OpCodes {
 
 }
     
+impl Intel4004 {
+    pub fn new() -> Intel4004 {
+
+        let reg = Registers {
+            pc: 0x00,
+            carry: false,
+            acc: 0x00,
+            index: [0x00; 16],
+            stack: [0x00; 3],
+        };
+
+        Intel4004 {
+            registers: reg,
+        }
+    }
+}
+
 fn cycle() {
 
     fetch_instruction();
@@ -31,9 +48,10 @@ fn decode_op() {
 // Intel 4004 registers 
 
 struct Registers {
-    index: [u8; 16],      // Dynamic RAM cell array of 16 x 4 bits.
-    acc:   u8,
     pc:    u8,
+    carry: bool, 
+    acc:   u8,
+    index: [u8; 16],      // Dynamic RAM cell array of 16 x 4 bits.
     stack: [u16; 3],     // 3 x 12 bits array
 }
 
@@ -42,9 +60,30 @@ struct Registers {
 
 fn print_registers(r: &Registers) {
 
-    // Print stack 
-    println!("Stack: /n Level 1: {:#02X} /n Level 2: {:#02X} /n Level 3: {:#02X}", r.stack[0], r.stack[1], r.stack[2] );
+    // Program Counter
+    println!("PC: {} ", r.pc);
 
+    // Carry
+    println!("\nCarry: {}", r.carry);
+
+    // Accumulator
+    println!("\nAccumulator: {:#01X}", r.acc);
+
+    // Index
+    println!("\nIndex: ");
+
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[0] , r.index[1] , r.index[2] , r.index[3] );
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[4] , r.index[5] , r.index[6] , r.index[7] );
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[8] , r.index[9] , r.index[10], r.index[11]);
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[12], r.index[13], r.index[14], r.index[15]);
+
+    // Stack 
+    println!("\nStack:");
+    println!(" Level 1: {:#02X}", r.stack[0]);
+    println!(" Level 2: {:#02X}", r.stack[1]);
+    println!(" Level 3: {:#02X}", r.stack[2]);
+
+    
 }
 
 // TODO: add ROM mudule(Intel 4001) and RAM module
@@ -53,5 +92,5 @@ fn main() {
 
     // TODO; Initialize cpu in order to test function
     
-    //print_registers();
+    print_registers(&Intel4004::new().registers);
 }
