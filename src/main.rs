55488bf7adc2acac4c7884b2ff2f81a7ecb3a22f
JaimeNA <nam.jaime@gmail.com>
2022-12-02 17:43:29 -0300

    Started with operation codes

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,85 @@
 // Intel 4004
 
 struct Intel4004 {
-    registers: Registers,
+    pc:    u8,
+    carry: bool, 
+    acc:   u8,
+    index: [u8; 16],      // Dynamic RAM cell array of 16 x 4 bits.
+    stack: [u16; 3],     // 3 x 12 bits array
 }
-
-enum OpCodes {
-
-}
-    
+  
 impl Intel4004 {
     pub fn new() -> Intel4004 {
+        Intel4004 {
+            pc: 0x00,
+            carry: false,
+            acc: 0x00,
+            index: [0x00; 16],
+            stack: [0x00; 3],
+        }
+    }
 
-        let reg = Registers {
+    // TODO: Implement clock function
+    
+    fn reset(&mut self) {
+        *self = Intel4004 {
             pc: 0x00,
             carry: false,
             acc: 0x00,
             index: [0x00; 16],
             stack: [0x00; 3],
         };
+    }
 
-        Intel4004 {
-            registers: reg,
+    fn fetch_instruction() {
+
+    }
+
+    // 1-word instructions take 1 instruction cycle while 2-word intructions take 2.
+    fn decode_op(&mut self, op_code: u16) {
+        match op_code{
+            // Machine instructions
+            0x0000 => self.nop()       ,
+            0x1000 => self.jnc(op_code), // 2-word instruction
+            0x2000 => self.fim(op_code), // 2-word instruction 
+            0x2100 => self.src(op_code),
+            0x3000 => self.src(op_code),
+            _ => self.pc += 3
         }
+    }
+
+    // Instructions
+
+    // No operation.
+    fn nop(&mut self) {
+        self.pc += 1;
+    }
+
+    // Jump to ROM address X if condition is true, otherwise skip.
+    fn jnc(&mut self, op_code: u16) {
+
+        // TODO: internal magic
+
+        self.pc += 2;
+    }
+
+    // Fetch immediate from ROM data X to specified index register pair.
+    fn fim(&mut self, op_code: u16) {
+
+        // TODO: internal magic
+
+        self.pc += 2;
+    }
+
+    // Send register control. Send the context of the specified index pair to ROM and RAM at set time.
+    fn src(&mut self, op_code: u16) {
+
+        // TODO: internal magic
+
+        self.pc += 1;
     }
 }
 
-fn cycle() {
-
-    fetch_instruction();
-    decode_op();
-
-}
-fn reset() {
-
-}
-
-fn fetch_instruction() {
-
-}
-
-fn decode_op() {
-        
-}
 
 
 // Intel 4004 registers 
@@ -58,30 +96,30 @@ struct Registers {
 
 // Desassembler
 
-fn print_registers(r: &Registers) {
+fn print_cpu_state(cpu: &Intel4004) {
 
     // Program Counter
-    println!("PC: {} ", r.pc);
+    println!("PC: {} ", cpu.pc);
 
     // Carry
-    println!("\nCarry: {}", r.carry);
+    println!("\nCarry: {}", cpu.carry);
 
     // Accumulator
-    println!("\nAccumulator: {:#01X}", r.acc);
+    println!("\nAccumulator: {:#01X}", cpu.acc);
 
     // Index
     println!("\nIndex: ");
 
-    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[0] , r.index[1] , r.index[2] , r.index[3] );
-    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[4] , r.index[5] , r.index[6] , r.index[7] );
-    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[8] , r.index[9] , r.index[10], r.index[11]);
-    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", r.index[12], r.index[13], r.index[14], r.index[15]);
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", cpu.index[0] , cpu.index[1] , cpu.index[2] , cpu.index[3] );
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", cpu.index[4] , cpu.index[5] , cpu.index[6] , cpu.index[7] );
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", cpu.index[8] , cpu.index[9] , cpu.index[10], cpu.index[11]);
+    println!(" {:#01X} {:#01X} {:#01X} {:#01X}", cpu.index[12], cpu.index[13], cpu.index[14], cpu.index[15]);
 
     // Stack 
     println!("\nStack:");
-    println!(" Level 1: {:#02X}", r.stack[0]);
-    println!(" Level 2: {:#02X}", r.stack[1]);
-    println!(" Level 3: {:#02X}", r.stack[2]);
+    println!(" Level 1: {:#02X}", cpu.stack[0]);
+    println!(" Level 2: {:#02X}", cpu.stack[1]);
+    println!(" Level 3: {:#02X}", cpu.stack[2]);
 
     
 }
@@ -90,7 +128,10 @@ fn print_registers(r: &Registers) {
 
 fn main() {
 
-    // TODO; Initialize cpu in order to test function
-    
-    print_registers(&Intel4004::new().registers);
+    let mut cpu = Intel4004::new();
+
+    cpu.decode_op(0x1000);
+    print_cpu_state(&cpu);
+
+
 }
