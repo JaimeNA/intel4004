8bcbdab247ce047d56f50f56560bf20dd4e338a1
JaimeNA <nam.jaime@gmail.com>
2022-12-26 10:49:58 -0300

    Finished

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::{thread, time};
 
 use intel4004_emu::intel4004::Intel4004;
 use intel4004_emu::disassembler::*;
@@ -7,18 +8,25 @@ fn main() -> io::Result<()>{
 
     let mut cpu = Intel4004::new();
 
-    cpu.rom.load_rom("rom/RDn")?;
+    cpu.rom.load_rom("rom/ram_test")?;
 
     let mut i = 0;
+    
+    let delay = time::Duration::from_millis(10);
+    let mut now = time::Instant::now();
 
-    while i < 50 {
+    while i < 5000 {
+        now = time::Instant::now();
+
         cpu.clock();
+
+        print_rom(&cpu.rom);
+        print_ram(&cpu.ram);
+        print_cpu_state(&cpu);
+
+        thread::sleep(delay);
         i += 1;
     }
-
-    print_rom(&cpu.rom);
-    print_ram(&cpu.ram);
-    print_cpu_state(&cpu);
 
     Ok(())
 }
